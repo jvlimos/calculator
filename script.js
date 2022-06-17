b0d2a48beb2d8436fd783e74ea56665952830ab8
jvlimos <johnvincentlimos@yahoo.com>
2022-06-17 21:06:27 +0800

    Add buttons and css foundation

---
+++ script.js
@@ -0,0 +1,24 @@
+* {
+  border: 1px red solid;
+}
+
+:root {
+	font-size: 62.5%;
+}
+* {
+  box-sizing: border-box;
+  font-family: ;
+/*	margin: 0;
+	padding: 0; */
+}
+*::before, *::after {
+    box-sizing: inherit;
+}
+html {
+  scroll-behavior: smooth;
+}
+body {
+	margin: 0;
+	font-size: 1.6rem;
+	font-family: ;
+}
